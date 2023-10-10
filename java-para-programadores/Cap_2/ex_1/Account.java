dc015b430bcd9d122f700c38a082cac25e65b5bc
Davi dos Santos <davisousa7589@gmail.com>
2023-10-10 17:01:06 -0300

    a

--- java-para-programadores/Cap_2/ex_1/Account.java
+++ java-para-programadores/Cap_2/ex_1/Account.java
@@ -1,14 +1,14 @@
-public class Account 
-{
-    private String name; 
+public class Account {
+    private String name; // declarado uma variável to tipo string de escopo privado, só pode ser acessível aos métodos declarados nesta classe
 
-    public void setName(String name)
+    public void setName(String name) // passagem de parâmetros? método void não retornará nenhema informação ao seu chamador, este método apenas armazena dados
     {
-        this.name = name; 
+        this.name = name;
     }
 
-    public String getName() 
+    public String getName() // o tipo de dado retornado ao chamador será uma string, método que retorna string
     {
-        return name; 
+        return name;
     }
-} 
+
+}
