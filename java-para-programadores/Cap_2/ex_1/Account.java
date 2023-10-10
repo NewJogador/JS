26db6815f88afc7e805163dce6a69101a460c244
Davi dos Santos <davisousa7589@gmail.com>
2023-10-10 16:13:08 -0300

    Comentários removidos

--- java-para-programadores/Cap_2/ex_1/Account.java
+++ java-para-programadores/Cap_2/ex_1/Account.java
@@ -1,20 +1,14 @@
-// Figura 3.1: Account.java
-// Classe Acount que contém uma variável de instância name
-// e métodos para configurar e obter seu valor
-
 public class Account 
 {
-    private String name; // variável de instância
+    private String name; 
 
-    //método para definir o nome no objeto
     public void setName(String name)
     {
-        this.name = name; //armazena o nome
+        this.name = name; 
     }
 
-    //método para recuperar o nome do objeto
     public String getName() 
     {
-        return name; // retorna valor do nome para o chamador
+        return name; 
     }
-} // fim da classe Account
+} 
